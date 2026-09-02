6507bb9a2acadef6ea7c465e7bd2035ff9815063
Keycy <keycyjmr30@gmail.com>
2026-09-02 16:14:54 -0500

    Se agregó la cabera de la clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a, b, c;
 
-public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int a, b, c;
-
-    System.out.println("Ingrese el valor de A:");
-    a = Integer.parseInt(br.readLine());
-    System.out.println("Ingrese el valor de B:");
-    b = Integer.parseInt(br.readLine());
-    System.out.println("Ingrese el valor de C:");
-    c = Integer.parseInt(br.readLine());
+        System.out.println("Ingrese el valor de A:");
+        a = Integer.parseInt(br.readLine());
+        System.out.println("Ingrese el valor de B:");
+        b = Integer.parseInt(br.readLine());
+        System.out.println("Ingrese el valor de C:");
+        c = Integer.parseInt(br.readLine());
+    }
 }
